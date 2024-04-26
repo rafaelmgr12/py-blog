ffd3c690fae0500d7ae60291b037b25cefaef4e9
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-26 17:01:09 -0300

    Add BlogPost entity class with properties and validation

---
+++ src/domain/entity/blog_post.py
@@ -0,0 +1,43 @@
+import datetime
+import uuid
+
+
+class BlogPost:
+
+    __id: uuid.UUID
+    __title: str
+    __content: str
+    __author_id: uuid.UUID
+    __created_at: datetime
+    __updated_at: datetime
+    
+    
+    def __init__(self, title: str, content: str)->str:
+        self.__id = uuid.uuid4()
+        self.__title = title
+        self.__content = content
+        self.__created_at = datetime.datetime.now()
+        self.__updated_at = datetime.datetime.now()
+        
+        self._validate()   
+        
+    def _validate(self):
+        if not self.__title:
+            raise ValueError("Title is required")
+        if not self.__content:
+            raise ValueError("Content is required")
+        if not self.__author_id:
+            raise ValueError("Author is required")
+        
+    @property
+    def id(self)-> uuid.UUID:
+        return self.__id
+    
+    @property
+    def title(self)-> str:
+        return self.__title
+    
+    @property
+    def content(self)-> str:
+        return self.__content
+    
