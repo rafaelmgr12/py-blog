93fe9937581f85b07b090e1d8909028e2e2ce438
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-27 19:56:13 -0300

    [feat] Add methods to BlogPostUseCase class for getting all blog posts by user ID, updating a blog post, and deleting a blog post

--- src/app/usecase/blog_post_usecase.py
+++ src/app/usecase/blog_post_usecase.py
@@ -1,3 +1,4 @@
+from typing import List
 import uuid
 
 from src.domain.entity.blog_post import BlogPost
@@ -32,4 +33,52 @@ class BlogPostUseCase:
             return self.blog_post_repository.get_blog_post_by_id(blog_post_id_uuid)
         except Exception as e:
             raise e
+        
+        
+    def get_all_blog_post_by_user_id(self, user_id: str)-> List[BlogPost] | List[None]:
+        user_id_uuid = uuid.UUID(user_id)
+        try:
+            return self.blog_post_repository.get_all_blog_post_by_user_id(user_id_uuid)
+        except Exception as e:
+            raise e
+        
+        
+    def update_blog_post(self, blog_post_id: str, title: str, content: str, user_id: str)-> BlogPost:
+        blog_post_id_uuid = uuid.UUID(blog_post_id)
+        user_id_uuid = uuid.UUID(user_id)
+        blog_post = self.blog_post_repository.get_blog_post_by_id(blog_post_id_uuid)
+        
+        if not blog_post:
+            raise ValueError("Blog post not found")
+        
+        if blog_post.author_id != user_id_uuid:
+            raise ValueError("User not authorized")
+        
+        blog_post.title = title
+        blog_post.content = content
+        
+        try:
+            return self.blog_post_repository.update_blog_post(blog_post)
+        except Exception as e:
+            raise e
+        
+    def delete_blog_post(self, blog_post_id: str, user_id: str)-> None:
+        blog_post_id_uuid = uuid.UUID(blog_post_id)
+        user_id_uuid = uuid.UUID(user_id)
+        blog_post = self.blog_post_repository.get_blog_post_by_id(blog_post_id_uuid)
+        
+        if not blog_post:
+            raise ValueError("Blog post not found")
+        
+        
+        print(f"Attempting to delete blog post by user: {user_id_uuid}, Blog post author: {blog_post.author_id}")
+
+        if blog_post.author_id != user_id_uuid:
+            raise ValueError("User not authorized")
+        
+        try:
+            self.blog_post_repository.delete_blog_post(blog_post_id_uuid)
+        except Exception as e:
+            raise e
+        
         
