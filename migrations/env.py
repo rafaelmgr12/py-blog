513b734f04a69a1e021172a6c4bfcb40f4aadd6a
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-28 14:47:28 -0300

    [refactor] Update migrations/env.py to use environment variables for database connection

--- migrations/env.py
+++ migrations/env.py
@@ -5,8 +5,21 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from src.infra.db.models.blog_post_model import Base
+from src.infra.db.models.user_model import Base
+from src.infra.db.db import Base
+
 from alembic import context
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+db_name = os.getenv("POSTGRES_DB")
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -15,12 +28,13 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
+config.set_main_option(
+    "sqlalchemy.url",f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}")
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
