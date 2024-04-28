a705879837efbf14260f55005d21408cbe7e6f18
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-28 13:26:01 -0300

    [refactor] Update main.py to use environment variables for database connection and server configuration

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-from src.infra.web.webserver import WebServer
+import os
 
+from dotenv import load_dotenv
+
+from src.app.usecase.user_usecase import UserUsecase
+from src.infra.db.db import DBConnect
+from src.infra.repository.user_repo import SQLUserRepository
+from src.infra.web.handler.user_handler import UserHandler
+from src.infra.web.webserver.webserver import WebServer
+import asyncio
+
+load_dotenv()
+
+
+
+
+async def main(conn_str: str, port: int, host: str)-> None:
+    # Initialize the database connection
+    db_connect = DBConnect(connection_string=conn_str)  # Make sure to provide the actual connection string
+
+    # Create session for the user repository
+    async with db_connect.get_session() as session:
+        user_repo = SQLUserRepository(session)
+        user_usecase = UserUsecase(user_repo)
+        user_handler = UserHandler(user_usecase)
+
+        server = WebServer(port=port, host=host)
+
+        # Add your existing routes
+        def example_route():
+            return {"message": "Wlecome to the server!"}
+        server.add_route("/", example_route, ["GET"])
+
+        # Add the create_user route
+        server.add_route("/create_user", user_handler.create_user, ["POST"])
+
+        # Run the server
+        server.run()
 if __name__ == "__main__":
-    server = WebServer(port=8080)
-    def example_route():
-        return {"message": "Dynamic route response"}
+    conn = os.getenv("POSTGRES_URL")
+    port = os.getenv("PORT")
+    host = os.getenv("HOST")
 
-    server.add_route("/dynamic", example_route, ["GET", "POST"])
-    server.run()
+    async def main_wrapper():
+        await main(conn, port, host)
+
+    asyncio.run(main_wrapper())
