877a473d05018cccef2d6e1d20fad436fc819175
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-28 15:18:08 -0300

    [refactor] Update UserUsecase to use asynchronous methods

--- src/app/usecase/user_usecase.py
+++ src/app/usecase/user_usecase.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from src.domain.entity.user import User
 from src.domain.ports.user import UserPort
 
@@ -7,46 +8,46 @@ class UserUsecase:
     def __init__(self, user_repository: UserPort):
         self.user_repository = user_repository
 
-    def create_user(self, name: str, email: str, password: str) -> User:
+    async def create_user(self, name: str, email: str, password: str) -> User:
         user = User(name, email, password)
         user.hash_password()
 
-        if self.user_repository.get_user_by_email(email):
+        if await self.user_repository.find_by_email(email):
             raise ValueError("Email already exists")
 
         try:
-            self.user_repository.create_user(user)
+            await self.user_repository.create(user)
         except Exception as e:
-            raise e
+            raise str(e)
 
         return user
 
-    def get_user_by_email(self, email: str) -> User | None:
+    async def get_user_by_email(self, email: str) -> User | None:
         try:
-            return self.user_repository.get_user_by_email(email)
+            return await self.user_repository.find_by_email(email)
         except Exception as e:
             raise e
         
-    def get_user_by_id(self, user_id: int) -> User | None:
+    async def get_user_by_id(self, user_id: int) -> User | None:
         try:
-            return self.user_repository.get_user_by_id(user_id)
+            return await self.user_repository.find_by_id(user_id)
         except Exception as e:
             raise e
         
         
-    def update_user(self, user_id: int, name: str, email: str, password: str) -> User:
+    async def update_user(self, user_id: int, name: str, email: str, password: str) -> User:
         user = User(name, email, password)
         user.hash_password()
 
         try:
-            self.user_repository.update_user(user_id, user)
+            await self.user_repository.update(user_id, user)
         except Exception as e:
             raise e
 
         return user
     
-    def delete_user(self, user_id: int) -> None:
+    async def delete_user(self, user_id: int) -> None:
         try:
-            self.user_repository.delete_user(user_id)
+            await self.user_repository.delete(user_id)
         except Exception as e:
             raise e
