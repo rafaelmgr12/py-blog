d18a9f4fb611a08f96663151a46cd99a80e81a63
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-30 18:19:17 -0300

    Refactor user_usecase.py to use async methods and update unit tests

--- tests/unit/test_user_usecase.py
+++ tests/unit/test_user_usecase.py
@@ -1,72 +1,102 @@
-import unittest
-from unittest.mock import Mock, MagicMock
-from src.domain.entity.user import User
+import pytest
 from src.app.usecase.user_usecase import UserUsecase
+from src.domain.entity.user import User
+from unittest.mock import AsyncMock, MagicMock
 
-class TestUserUsecase(unittest.TestCase):
+@pytest.fixture
+def user_usecase():
+    user_repository = MagicMock()
+    user_repository.find_by_email = AsyncMock()
+    user_repository.create = AsyncMock()
+    user_repository.update = AsyncMock()
+    user_repository.delete = AsyncMock()
+    user_repository.find_by_id = AsyncMock()
+    return UserUsecase(user_repository)
 
-    def setUp(self):
-        self.user_repository = Mock()
-        self.user_usecase = UserUsecase(self.user_repository)
+@pytest.mark.asyncio
+async def test_create_user_success(user_usecase):
+    name = "Test User"
+    email = "test@example.com"
+    password = "password"
+    
+    user_usecase.user_repository.find_by_email.return_value = None
+    user_usecase.user_repository.create.return_value = User(name, email, password)
+    
+    user = await user_usecase.create_user(name, email, password)
+    
+    assert user.name == name
+    assert user.email == email
+    assert user.password != password  # Verifica se a senha foi hashada
 
-    def test_create_user(self):
-        name = "Test User"
-        email = "test@example.com"
-        password = "password"
+@pytest.mark.asyncio
+async def test_create_user_failure_existing_email(user_usecase):
+    email = "test@example.com"
+    user_usecase.user_repository.find_by_email.return_value = User("Existing User", email, "password")
+    
+    with pytest.raises(ValueError) as exc_info :
+        error = await user_usecase.create_user("Test User", email, "new_password")
 
-        self.user_repository.get_user_by_email.return_value = None
-        self.user_repository.create_user.return_value = User(name, email, password)
+    assert user_usecase.user_repository.create.call_count == 0
+    assert str(exc_info.value) == "Email already exists"
+    
+@pytest.mark.asyncio
+async def test_get_user_by_email_found(user_usecase):
+    email = "test@example.com"
+    expected_user = User("Test User", email, "password")
+    user_usecase.user_repository.find_by_email.return_value = expected_user
+    
+    user = await user_usecase.get_user_by_email(email)
+    
+    assert user == expected_user
 
-        user = self.user_usecase.create_user(name, email, password)
+@pytest.mark.asyncio
+async def test_get_user_by_email_not_found(user_usecase):
+    email = "notfound@example.com"
+    user_usecase.user_repository.find_by_email.return_value = None
+    
+    user = await user_usecase.get_user_by_email(email)
+    
+    assert user is None
 
-        self.assertEqual(user.name, name)
-        self.assertEqual(user.email, email)
+@pytest.mark.asyncio
+async def test_get_user_by_id_found(user_usecase):
+    user_id = 1
+    expected_user = User("Test User", "test@example.com", "password")
+    user_usecase.user_repository.find_by_id.return_value = expected_user
+    
+    user = await user_usecase.get_user_by_id(user_id)
+    
+    assert user == expected_user
 
-    def test_get_user_by_email(self):
-        email = "test@example.com"
-        self.user_repository.get_user_by_email.return_value = User("Test User", email, "password")
+@pytest.mark.asyncio
+async def test_get_user_by_id_not_found(user_usecase):
+    user_id = 999
+    user_usecase.user_repository.find_by_id.return_value = None
+    
+    user = await user_usecase.get_user_by_id(user_id)
+    
+    assert user is None
 
-        user = self.user_usecase.get_user_by_email(email)
+@pytest.mark.asyncio
+async def test_update_user_success(user_usecase):
+    name = "Updated User"
+    email = "updated@example.com"
+    password = "updated_password"
+    
+    updated_user = User("Teste", "old@example.com", "test123")
+    user_usecase.user_repository.update.return_value = updated_user
+    
+    user = await user_usecase.update_user(updated_user.id, name, email, password)
+    
+    assert user.name == name
+    assert user.email == email
+    assert user.password != password
+    
 
-        self.assertEqual(user.email, email)
-
-    def test_get_user_by_id(self):
-        user_test = self.user_repository.get_user_by_id.return_value = User("Test User", "test@example.com", "password")
-
-        user = self.user_usecase.get_user_by_id(user_test.id)
-
-        self.assertEqual(user.id, user_test.id)
-
-    def test_update_user(self):
-        user_id = 1
-        name = "Updated User"
-        email = "updated@example.com"
-        password = "updated_password"
-
-        self.user_repository.update_user.return_value = User(name, email, password)
-
-        user = self.user_usecase.update_user(user_id, name, email, password)
-
-        self.assertEqual(user.name, name)
-        self.assertEqual(user.email, email)
-        
-
-    def test_delete_user(self):
-        user_id = 1
-
-        self.user_usecase.delete_user(user_id)
-
-        self.user_repository.delete_user.assert_called_once_with(user_id)
-        
-    def test_create_user_with_existing_email(self):
-        email = "test@test.com"
-        self.user_repository.get_user_by_email.return_value = User("Test User", email, "password")
-        
-        with self.assertRaises(ValueError):
-            self.user_usecase.create_user("Test User", email, "password")
-            
-            
-        
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.asyncio
+async def test_delete_user_success(user_usecase):
+    user_id = 1
+    
+    await user_usecase.delete_user(user_id)
+    
+    user_usecase.user_repository.delete.assert_called_once_with(user_id)
