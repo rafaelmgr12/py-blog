394ecde49f6490f44b5968522212ec2ee1c218ff
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-30 15:24:54 -0300

    [refactor] include responses in the logs

--- src/infra/web/middleware/logs_middleware.py
+++ src/infra/web/middleware/logs_middleware.py
@@ -1,13 +1,20 @@
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
+from starlette.responses import Response
 from datetime import datetime
 import logging
 
 class LoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
         start_time = datetime.now()
         response = await call_next(request)
         process_time = (datetime.now() - start_time).total_seconds()
+
+        body = b''
+        async for chunk in response.body_iterator:
+            body += chunk
+        
+        new_response = Response(content=body, status_code=response.status_code, headers=dict(response.headers))
 
         logger = logging.getLogger("webserver_logger")
         log_record = {
@@ -16,11 +23,8 @@ class LoggingMiddleware(BaseHTTPMiddleware):
             "remoteAddr": request.client.host,
             "processTime": process_time,
             "status_code": response.status_code,
-            "message": response.body.decode("utf-8")
+            "response": body.decode()  #
         }
         logger.info(log_record)
-        
 
-        return response
-    
-    
+        return new_response  
